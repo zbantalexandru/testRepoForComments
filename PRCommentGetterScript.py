ba9eb08231c98e22ae3e556edd8a6b7ad89757b1
Zbant Gabriel Alexandru <60919382+zbantalexandru@users.noreply.github.com>
2023-07-04 21:23:11 +0300

    first commit

---
+++ PRCommentGetterScript.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_data(api):
+        response = requests.get(api)
+        if response.status_code == 200:
+            print("sucessfully fetched the data")
+            print(response.content)
+        else:
+            print("Acest api a cedat stresului :(")
+
+get_data("https://api.github.com/repos/zbantalexandru/testRepoForComments/pulls/comments");
+print(2+2);
